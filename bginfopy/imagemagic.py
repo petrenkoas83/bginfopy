f7fb137688ba7d19fc75b8880f4b1e1b44ca33d3
0-d <zubastik88@gmail.com>
2017-02-28 14:16:05 +0300

    Minor refactoring

--- bginfopy/imagemagic.py
+++ bginfopy/imagemagic.py
@@ -1,11 +1,12 @@
 import subprocess
 from preferences import *
 
-#http://www.imagemagick.org/Usage/text/\
+
+# http://www.imagemagick.org/Usage/text/\
 def put_text(in_img, out_img):
     if VERBOSE: print("Input image: '{0}'; Output image: '{1}'.".format(in_img, out_img))
-    #http://stackoverflow.com/questions/25079140/python-subprocess-popen-check-for-success-and-errors
-    #https://docs.python.org/2/library/subprocess.html#subprocess.check_call
+    # http://stackoverflow.com/questions/25079140/python-subprocess-popen-check-for-success-and-errors
+    # https://docs.python.org/2/library/subprocess.html#subprocess.check_call
     try:
         if (out_img == '') or not USE_WALLPAPER_IMAGE:
             if VERBOSE: print("(out_img == '') or not USE_WALLPAPER_IMAGE")
@@ -14,12 +15,12 @@ def put_text(in_img, out_img):
             #       -size 165x70 -gravity center label:Anthony     label_color.gif
             output = subprocess.check_output(["convert",
                                               "-background", BACKGROUND_COLOR,
-                                              "-fill","dodgerblue",
+                                              "-fill", "dodgerblue",
                                               "-font", "Candice",
                                               "-strokewidth", "2",
-                                              "-stroke","blue",
-                                              "-undercolor","lightblue",
-                                              "-size","165x70",
+                                              "-stroke", "blue",
+                                              "-undercolor", "lightblue",
+                                              "-size", "165x70",
                                               "-gravity", TEXT_GRAVITY,
                                               "label:'Test text'",
                                               out_img],
@@ -29,15 +30,16 @@ def put_text(in_img, out_img):
             # os.popen("convert {0} -gravity {2} -pointsize 30 -annotate +0+100 'TestText' {1}".format(in_img, out_img, TEXT_GRAVITY))
             output = subprocess.check_output(["convert",
                                               in_img,
-                                              "-gravity",TEXT_GRAVITY,
-                                              "-pointsize","30",
-                                              "-annotate","+0+100",
+                                              "-gravity", TEXT_GRAVITY,
+                                              "-pointsize", "30",
+                                              "-annotate", "+0+100",
                                               "label:'Test text'",
                                               out_img],
                                              stderr=subprocess.STDOUT)
+
     except subprocess.CalledProcessError as error:
-        #self.isCommandExecutionSuccessful = False
+        # self.isCommandExecutionSuccessful = False
         print("Error: '{0}': '{1}': '{2}'".format(error.returncode, error.output, error.message))
         return error.returncode
-    return 0
 
+    return 0
--- bginfopy/wallpapers.py
+++ bginfopy/wallpapers.py
@@ -2,48 +2,63 @@ import sys
 import os
 from preferences import *
 
+
 def get_wallpaper():
-    picture=''
+    picture = ''
     # http://stackoverflow.com/questions/2035657/what-is-my-current-desktop-environment
     determine_platform()
     desktop_session = determine_desktop_session()
-    if desktop_session in ["mate","mate-session"]:
-        picture=get_wallpaper_mate()
-    elif desktop_session in ["lubuntu","lxsession"]:
+
+    if desktop_session in ["mate", "mate-session"]:
+        picture = get_wallpaper_mate()
+    elif desktop_session in ["lubuntu", "lxsession"]:
         picture = get_wallpaper_lxde()
     else:
         sys.exit("Unknown desktop session: '{0}'".format(desktop_session))
+
     picture = picture.rstrip('\n\r')
     picture = picture.strip("'")
+
     if VERBOSE: print("Wallpaper: '{0}'".format(picture))
-    return(picture)
+    return picture
+
 
 def get_wallpaper_mate():
     return os.popen("gsettings get org.mate.background picture-filename").read()
 
+
 def get_wallpaper_lxde():
-    profile_name=os.popen("grep -i \"^@pcmanfm\" /etc/xdg/lxsession/Lubuntu/autostart | grep -oP \"\\-\\-profile[\\s=].*[\\s\\n\\r]*\"").read()
+    profile_name = os.popen(
+        "grep -i \"^@pcmanfm\" /etc/xdg/lxsession/Lubuntu/autostart | grep -oP \"\\-\\-profile[\\s=].*[\\s\\n\\r]*\""
+    ).read()
+
     if VERBOSE: print("Profile name: '{0}'".format(profile_name))
     if (profile_name is not None) and (profile_name <> ''):
-        #return os.popen("grep -i \"^wallpaper=\" $HOME/.config/pcmanfm/{0}/pcmanfm.conf | cut -d = -f2".format(profile_name)).read()
-        cmd="grep -i \"^wallpaper=\" $HOME/.config/pcmanfm/{0}/desktop-items-0.conf | cut -d = -f2".format(profile_name)
+        # return os.popen("grep -i \"^wallpaper=\" $HOME/.config/pcmanfm/{0}/pcmanfm.conf | cut -d = -f2".format(profile_name)).read()
+        cmd = "grep -i \"^wallpaper=\" $HOME/.config/pcmanfm/{0}/desktop-items-0.conf | cut -d = -f2".format(
+            profile_name)
     else:
         # return os.popen("grep -i \"^wallpaper=\" $HOME/.config/pcmanfm/LXDE/pcmanfm.conf | cut -d = -f2").read()
-        cmd="grep -i \"^wallpaper=\" $HOME/.config/pcmanfm/lubuntu/desktop-items-0.conf | cut -d = -f2"
+        cmd = "grep -i \"^wallpaper=\" $HOME/.config/pcmanfm/lubuntu/desktop-items-0.conf | cut -d = -f2"
+
     if VERBOSE: print("Command to get wallpaper: '{0}'".format(cmd))
     picture = os.popen(cmd).read()
-    return(picture)
+
+    return picture
+
 
 def set_wallpaper(out_img):
     result = -1
     determine_platform()
     desktop_session = determine_desktop_session()
+
     if desktop_session in ["mate", "mate-session"]:
         result = set_wallpaper_mate()
     elif desktop_session in ["lubuntu", "lxsession"]:
         result = set_wallpaper_lxde()
     else:
         sys.exit("Unsupported desktop session: '{0}'".format(desktop_session))
+
     return result
 
 
@@ -68,11 +83,13 @@ def determine_platform():
 
 def determine_desktop_session():
     desktop_session = os.environ.get("DESKTOP_SESSION")
+
     if desktop_session is not None:
         desktop_session = desktop_session.lower()
     else:
         desktop_session = os.popen("ps -u $USER | grep -ioP '\S+session' | grep -v grep").read()
         desktop_session = desktop_session.rstrip('\n')
+
     if desktop_session is not None:
         if VERBOSE: print('Desktop session: {0}'.format(desktop_session))
         return desktop_session
