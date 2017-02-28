03322b10a4ad1d24299b93c8853fc4281e62b9bd
Alexandr Petrenko <petrenkoas83@gmail.com>
2017-02-28 13:33:45 +0300

    Corrected some errors in processing of files of wallpapers.

--- bginfopy/__init__.py
+++ bginfopy/__init__.py
@@ -4,7 +4,7 @@ import imagemagic
 import os.path
 from preferences import *
 
-class image:
+class Image:
     def __init__(self, full_path):
         self.full_path = full_path
     def directory_path(self):
@@ -21,25 +21,37 @@ class image:
 
 def main():
     global USE_WALLPAPER_IMAGE
+
+    # Create Image objects
+    in_img = Image(wallpapers.get_wallpaper())
+    out_img = Image(USER_CONF_DIR + "/" + in_img.file_name() + SUFFIX + "." + in_img.file_ext())
+
     # Check current wallpaper
-    in_img = image(wallpapers.get_wallpaper())
     if in_img.file_name().endswith(SUFFIX):
-        if VERBOSE and (ORIGINAL_WALLPAPER_IMAGE == ''): print("Can not find original wallpaper: '{0}'".format(ORIGINAL_WALLPAPER_IMAGE))
-        in_img = image(ORIGINAL_WALLPAPER_IMAGE)
+        # If current wallpaper created with bginfo, then try to get original wallpaper
+        if ORIGINAL_WALLPAPER_IMAGE == '':
+            # If original wallpaper is not set in config, then use blank background
+            if VERBOSE: print("Can not find original wallpaper: '{0}'".format(ORIGINAL_WALLPAPER_IMAGE))
+            in_img.full_path = ''
+        else:
+            # If original wallpaper is set in config, then use original wallpaper
+            in_img.full_path = ORIGINAL_WALLPAPER_IMAGE
+
+    # Check file of original wallpaper
     if in_img.file_exist():
         if VERBOSE: print('File {0} is exist!'.format(in_img.full_path))
-        out_img = image((USER_CONF_DIR + "/" + in_img.file_name() + SUFFIX + "." + in_img.file_ext()))
     else:
-        print('File {0} does not exist! I will use blank bagckround.'.format(in_img.full_path))
+        # If file of original wallpaper does not exist, then use blank background
+        if VERBOSE: print('File {0} does not exist! I will use blank background.'.format(in_img.full_path))
         USE_WALLPAPER_IMAGE = False
         in_img.full_path = ''
 
     # Process wallpaper image
     if VERBOSE: print("Try to process wallpaper image.")
     if imagemagic.put_text(in_img.full_path, out_img.full_path) == 0:
-        # Set new wallpaper
+        # In convert successfull, then set new wallpaper
         if wallpapers.set_wallpaper(out_img.full_path) == 0:
-            # Renew path to original wallpaper image
+            # If set new wallpaper is successfull, then renew path to original wallpaper image
             if VERBOSE: print("Previous value of [MAIN][ORIGINAL_WALLPAPER_IMAGE]: '{0}'".format(config['MAIN']['ORIGINAL_WALLPAPER_IMAGE']))
             config.set('MAIN', 'ORIGINAL_WALLPAPER_IMAGE', in_img.full_path)
             # Try to write user config
