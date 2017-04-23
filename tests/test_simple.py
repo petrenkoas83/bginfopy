a1ce0067cce56cbc26370d1c0f53902ebf3a8c56
Alexandr Petrenko <petrenkoas83@gmail.com>
2017-04-23 17:56:17 +0300

    test_image_class

--- tests/test_simple.py
+++ tests/test_simple.py
@@ -6,3 +6,10 @@ import __init__ as init
 def test_image_class():
     test_img = init.Image('')
     assert not test_img.file_exist
+    assert not test_img.file_name
+    test_img.full_path = os.path.join(sys.path[0], '../tests/test_simple.py')
+    assert test_img.file_exist
+    assert test_img.directory_path == os.path.join(sys.path[0], '../tests')
+    assert test_img.file_full_name == 'test_simple.py'
+    assert test_img.file_name == 'test_simple'
+    assert test_img.file_ext == '.py'
